0658a418ad95c97a9d56572288cbe87b384081a7
Khalid <191556389+Abduboriyxon@users.noreply.github.com>
2025-09-16 10:09:45 +0500

    init function

---
+++ basic/init.go
@@ -0,0 +1,19 @@
+package basic
+
+import "fmt"
+
+func init() {
+	println("Init function called1")
+}
+func init() {
+	println("Init function called2")
+}
+func init() {
+	println("Init function called3")
+}
+
+func main() {
+
+	fmt.Println("Main function called")
+
+} 
