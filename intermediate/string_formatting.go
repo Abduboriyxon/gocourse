7475321d294673b15cf8b2c62657b6b703229830
Khalid <191556389+Abduboriyxon@users.noreply.github.com>
2025-10-19 18:26:35 +0500

    string_formatting

---
+++ intermediate/string_formatting.go
@@ -0,0 +1,25 @@
+package intermediate
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	num := 424
+	fmt.Printf("%05d\n", num) // Output: 00424
+
+	message := "hello"
+	fmt.Printf("|%10s|\n", message)
+	fmt.Printf("|%-10s|\n", message) 
+
+	message1 := "Hello\nWorld"
+	message2 := `Hello\nWorld`
+
+	fmt.Println(message1)
+	fmt.Println(message2)
+
+	// sqlQuery := `SELECT * FROM users WHERE age > 30`
+
+
+} 
