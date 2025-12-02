17cb8a7e5c21c74c85435e48654dc11113b0de32
Khalid <191556389+Abduboriyxon@users.noreply.github.com>
2025-12-02 17:55:43 +0500

    Logging

---
+++ intermediate/logrus_logging.go
@@ -0,0 +1,25 @@
+package intermediate
+
+import "github.com/sirupsen/logrus"
+
+func main() {
+
+	log := logrus.New()
+
+	// set log level
+	log.SetLevel(logrus.InfoLevel)
+
+	// set log format
+	log.SetFormatter(&logrus.JSONFormatter{})
+
+	// logging examples
+	log.Info("This is an info message")
+	log.Warn("This is a warning message")
+	log.Error("This is an error message")
+
+	log.WithFields(logrus.Fields{
+		"username": "johndoe",
+		"method": "GET",
+	}).Info("User logged in.")
+
+}
---
+++ intermediate/zap_log.go
@@ -0,0 +1,23 @@
+package intermediate
+
+import (
+	"log"
+
+	"go.uber.org/zap"
+)
+
+func main() {
+
+	logger, err := zap.NewProduction()
+	if err != nil {
+		log.Println("Failed to initialize zap logger")
+	}
+
+	defer logger.Sync()
+
+	logger.Info("This is an info message")
+
+	logger.Info("User logged in", zap.String("username", "John Doe"), zap.String("method", "GET"))
+
+
+}
