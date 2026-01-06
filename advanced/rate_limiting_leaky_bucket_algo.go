22b1c5b3cf130ff2a07ed7bc54367d9ac9ff1b28
Khalid <191556389+Abduboriyxon@users.noreply.github.com>
2026-01-06 14:14:23 +0500

    rate limiting leaky bucket algorithm

---
+++ advanced/rate_limiting_leaky_bucket_algo.go
@@ -0,0 +1,75 @@
+package advanced
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+type LeakyBucket struct{
+	capacity int
+	leakRate time.Duration
+	tokens int
+	lastLeak time.Time
+	mu sync.Mutex
+}
+
+func NewLeakyBucket(capacity int, leakRate time.Duration) *LeakyBucket {
+	return &LeakyBucket{
+		capacity: capacity,
+		leakRate: leakRate,
+		tokens: capacity,
+		lastLeak: time.Now(),
+	}
+}
+
+func (lb *LeakyBucket) Allow() bool{
+	lb.mu.Lock()
+	defer lb.mu.Unlock()
+
+	now := time.Now()
+	elapsedTime := now.Sub(lb.lastLeak)
+	tokensToAdd := int(elapsedTime / lb.leakRate) // (0.2 / 0.5) result is 0.4 int value is 0
+	lb.tokens += tokensToAdd
+
+	if lb.tokens > lb.capacity{
+		lb.tokens = lb.capacity
+	}
+
+	lb.lastLeak = lb.lastLeak.Add(time.Duration(tokensToAdd) * lb.leakRate)
+	// lb.lastLeak = lb.lastLeak.Add(elapsedTime) // WRONG APPROACH
+
+	// elapsedTime = 1.3 sec
+	// initial lastLeak = 0 sec
+	// tokensToAdd = int(1.3 / 0.5) = int(2.6) = 2 tokens
+	// lb.lastLeak = lb.lastLeak + (time.Duration(tokensToAdd) * lb.leakRate) 0 + (2 * 0.5) = 1.0 sec
+	// lb.lastLeak = lb.lastLeak + elapsedTime 0 + 1.3
+
+	fmt.Printf("Tokens added: %d, Tokens subtracted: %d, Total Tokens: %d\n", tokensToAdd, 1, lb.tokens)
+	fmt.Printf("Last leak time: %v\n", lb.lastLeak)
+	if lb.tokens > 0{
+		lb.tokens--
+		return true
+	}
+	return false
+}
+
+func main() {
+	leakyBucketInst := NewLeakyBucket(5, 500*time.Millisecond)
+	var wg sync.WaitGroup
+	for range 10{
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			if leakyBucketInst.Allow(){
+				fmt.Println("Current time:", time.Now())
+				fmt.Println("Request allowed")
+			} else {
+				fmt.Println("Current time:", time.Now())
+				fmt.Println("Request denied")
+			}
+			time.Sleep(200 * time.Millisecond)
+			}()
+	}
+	wg.Wait()
+}
