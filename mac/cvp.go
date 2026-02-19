fd9649a2c60d6b9d602e380d3deb62f7480c64a9
Khalid <191556389+Abduboriyxon@users.noreply.github.com>
2026-02-19 20:53:17 +0500

    cvp

---
+++ mac/cvp.go
@@ -0,0 +1,45 @@
+package mac
+
+import (
+	"fmt"
+	"time"
+)
+
+func printNumbers() {
+	for i := range 5{
+		fmt.Println(time.Now())
+		fmt.Println(i)
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+func printLetters(){
+	for _, letter := range "abcde"{
+		fmt.Println(time.Now())
+		fmt.Println(string(letter))
+		time.Sleep(500 * time.Millisecond)
+	}
+}
+
+// func heavyTask(id int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	fmt.Printf("Task %d starting\n", id)
+// 	for range 100_000_000 {}
+// 	fmt.Println(time.Now())
+// 	fmt.Printf("Task %d completed\n", id)
+// }
+
+func main() {
+	go printNumbers()
+	go printLetters()
+	time.Sleep(3 * time.Second)
+
+	// numThreads := 4
+	// runtime.GOMAXPROCS(numThreads)
+	// var wg sync.WaitGroup
+	// for i := range numThreads{
+	// 	wg.Add(1)
+	// 	go heavyTask(i, &wg)
+	// }
+	// wg.Wait()
+}
