95ba4db79e7ada9c939419140bd3fc1f2f26ebd2
Khalid <191556389+Abduboriyxon@users.noreply.github.com>
2026-04-25 15:47:43 +0500

    cleaned

--- REST_API_GO/cmd/api/server.go
+++ REST_API_GO/cmd/api/server.go
@@ -12,30 +12,24 @@ import (
 func rootHandler (w http.ResponseWriter, r *http.Request) {
 
 	w.Write([]byte("Hello root route!"))
-	fmt.Println("Received request for root route")
 }
 
 func teachersHandler (w http.ResponseWriter, r *http.Request){
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello GET Method on teachers route!"))
-		fmt.Println("Received GET Method request for teachers route!")
 		return
 	case http.MethodPost:
 		w.Write([]byte("Hello POST Method on teachers route!"))
-		fmt.Println("Received POST Method request for teachers route!")
 		return
 	case http.MethodPut:
 		w.Write([]byte("Hello PUT Method on teachers route!"))
-		fmt.Println("Received put Method request for teachers route!")
 		return
 	case http.MethodPatch:
 		w.Write([]byte("Hello PATCH Method on teachers route!"))
-		fmt.Println("Received PATCH Method request for teachers route!")
 		return
 	case http.MethodDelete:
 		w.Write([]byte("Hello DELETE Method on teachers route!"))
-		fmt.Println("Received DELETE Method request for teachers route!")
 		return
 	}
 }
@@ -44,23 +38,18 @@ func studentsHandler (w http.ResponseWriter, r *http.Request){
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello GET Method on students route!"))
-		fmt.Println("Received GET Method request for students route!")
 		return
 	case http.MethodPost:
 		w.Write([]byte("Hello POST Method on students route!"))
-		fmt.Println("Received POST Method request for students route!")
 		return
 	case http.MethodPut:
 		w.Write([]byte("Hello PUT Method on students route!"))
-		fmt.Println("Received put Method request for students route!")
 		return
 	case http.MethodPatch:
 		w.Write([]byte("Hello PATCH Method on students route!"))
-		fmt.Println("Received PATCH Method request for students route!")
 		return
 	case http.MethodDelete:
 		w.Write([]byte("Hello DELETE Method on students route!"))
-		fmt.Println("Received DELETE Method request for students route!")
 		return
 	}
 }
@@ -69,23 +58,18 @@ func execsHandler(w http.ResponseWriter, r *http.Request){
 	switch r.Method {
 	case http.MethodGet:
 		w.Write([]byte("Hello GET Method on execs route!"))
-		fmt.Println("Received GET Method request for execs route!")
 		return
 	case http.MethodPost:
 		w.Write([]byte("Hello POST Method on execs route!"))
-		fmt.Println("Received POST Method request for execs route!")
 		return
 	case http.MethodPut:
 		w.Write([]byte("Hello PUT Method on execs route!"))
-		fmt.Println("Received put Method request for execs route!")
 		return
 	case http.MethodPatch:
 		w.Write([]byte("Hello PATCH Method on execs route!"))
-		fmt.Println("Received PATCH Method request for execs route!")
 		return
 	case http.MethodDelete:
 		w.Write([]byte("Hello DELETE Method on execs route!"))
-		fmt.Println("Received DELETE Method request for execs route!")
 		return
 	}
 }
